d77ac5f17a5a76fa9e594d85bd3e1c823ae38ca2
KarlJoones- <C13315991@mydit.ie>
2016-01-30 19:52:09 +0000

    Added alert fragments

---
+++ app/src/main/java/com/karl/fragments/MyAlertDialogFragment.java
@@ -0,0 +1,52 @@
+package com.karl.fragments;
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.karl.fyp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MyAlertDialogFragment extends DialogFragment {
+
+
+    public MyAlertDialogFragment() {
+        // Required empty public constructor
+    }
+
+    public static MyAlertDialogFragment newInstance(String title, String message){
+        MyAlertDialogFragment fragment = new MyAlertDialogFragment();
+        Bundle args = new Bundle();
+        args.putString("title", title);
+        args.putString("message", message);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        String title = getArguments().getString("title");
+        String message = getArguments().getString("message");
+
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create();
+    }
+
+}
---
+++ app/src/main/java/com/karl/fragments/MyListAlertDialogFragment.java
@@ -0,0 +1,65 @@
+package com.karl.fragments;
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.karl.fyp.NewManualEntryActivity;
+import com.karl.fyp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MyListAlertDialogFragment extends DialogFragment {
+
+
+    public MyListAlertDialogFragment() {
+        // Required empty public constructor
+    }
+
+    public static MyListAlertDialogFragment newInstance(int title){
+        MyListAlertDialogFragment fragment = new MyListAlertDialogFragment();
+        Bundle args = new Bundle();
+        args.putInt("title", title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        int title = getArguments().getInt("title");
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.select_dialog_item);
+        arrayAdapter.add(getString(R.string.main_activity_new_entry_barcode));
+        arrayAdapter.add(getString(R.string.main_activity_new_entry_manual));
+
+        return new AlertDialog.Builder(getActivity())
+                .setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setTitle(title)
+                .setAdapter(arrayAdapter, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String itemClicked = arrayAdapter.getItem(which);
+                        if(itemClicked.equals(getString(R.string.main_activity_new_entry_barcode))){
+                            getFragmentManager().beginTransaction().replace(R.id.content_frame, new LookupFragment()).commit();
+                        } else if (itemClicked.equals(getString(R.string.main_activity_new_entry_manual))){
+                            startActivity(new Intent(getActivity(), NewManualEntryActivity.class));
+                        }
+                    }
+                })
+                .create();
+    }
+}

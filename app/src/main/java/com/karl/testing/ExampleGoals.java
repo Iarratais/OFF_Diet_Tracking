56c69fee2a0e92fe48995a91fafaea364acff4dd
KarlJoones- <C13315991@mydit.ie>
2016-03-25 12:35:09 +0000

    Bug fixes

---
+++ app/src/main/java/com/karl/testing/ExampleGoals.java
@@ -0,0 +1,51 @@
+package com.karl.testing;
+
+/**
+ * Copyright Karl jones 2016.
+ *
+ * Example goals are used as a Mock Object in the testing of the application. This is set to
+ * return values that the application is expecting and is used for testing purposes only.
+ */
+
+public class ExampleGoals {
+    private String example_goal_calories = "2000";
+    private String example_goal_fat = "65";
+    private String example_goal_sat_fat = "20";
+    private String example_goal_sodium = "2.4";
+    private String example_goal_carbs = "300";
+    private String example_goal_protein = "50";
+    private String example_goal_sugar = "160";
+    private String example_goal_salt = "5";
+
+    public String getExample_goal_calories() {
+        return example_goal_calories;
+    }
+
+    public String getExample_goal_fat() {
+        return example_goal_fat;
+    }
+
+    public String getExample_goal_sat_fat() {
+        return example_goal_sat_fat;
+    }
+
+    public String getExample_goal_sodium() {
+        return example_goal_sodium;
+    }
+
+    public String getExample_goal_carbs() {
+        return example_goal_carbs;
+    }
+
+    public String getExample_goal_protein() {
+        return example_goal_protein;
+    }
+
+    public String getExample_goal_sugar() {
+        return example_goal_sugar;
+    }
+
+    public String getExample_goal_salt() {
+        return example_goal_salt;
+    }
+}
